ca4527269fcad761818f855d42f213705ad4f838
sathvik <sathvikchinnu@gmail.com>
2019-07-06 23:38:26 -0400

    Moved Metrics code to separate component

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -1,12 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useQuery } from 'urql';
-
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { Fragment, useState } from 'react';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -18,16 +10,7 @@ import {
   ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-import * as metricActions from '../../store/metrics/actions';
-
-const mockMetricsData = [
-  'casingPressure',
-  'oilTemp',
-  'waterTemp',
-  'flareTemp',
-  'injValveOpen',
-  'tubingPressure'
-];
+import Metrics from './Metrics';
 
 const mockChartData = [
   {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
@@ -56,88 +39,30 @@ const useStyles = makeStyles({
   }
 });
 
-const query = `query {getMetrics}`;
-
 const Dashboard = () => {
   const classes = useStyles();
-  const [state, setState] = useState([]);
-  
-  /****** Requesting Metrics Service and Storing it in Metrics Reducer ******/
-  const dispatch = useDispatch();
-  const [result] = useQuery({ query, });
 
-  const { fetching, data, error } = result;
+  const [state, setState] = useState({});
 
-  useEffect(() => {
-    if(!data) return;
-
-    const { getMetrics = [] } = data || {};
-
-    dispatch({ type: metricActions.GET_METRICS_DATA, metrics: getMetrics });
-  }, [dispatch, data]);
-  /****** Metrics Service Ending  ******/
-
-  // Get metrics data from `metrics` reducer
-  const metricsData = useSelector((store) => {
-    const { metrics } = store.metrics;
-
-    return metrics;
-  });
-
-  const handleChange = choiceName => (event) => {
-    const selectedChoices = [...state];
-  
-    if (event.target.checked) {
-      selectedChoices.push(choiceName);
-    } else {
-      const index = selectedChoices.indexOf(choiceName);
-  
-      if (index > -1) {
-        selectedChoices.splice(index, 1);
-      }
-    }
-  
-    setState([
-      ...selectedChoices,
-    ]);
+  const onMetricSelect = (selectedMetrics, metricsData) => {
+    setState({
+      selectedMetrics,
+      metricsData
+    });
   };
+
+  const {selectedMetrics, metricsData} = state;
 
   return (
     <Fragment>
       <div className={classes.dashboardContainer}>
 
-        {/* Checkboxes */}
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Select Metrics:</FormLabel>
-          <FormGroup row>
-            {
-              Object.keys(metricsData).map((metric) => {
-                const metricName = metric;
-                const metricLabelName = metricsData[metricName];
-
-                return (
-                  <FormControlLabel
-                    key={metric}
-                    control={(
-                      <Checkbox
-                        checked={state[metricName]}
-                        onChange={handleChange(metricName)}
-                        value={metricName}
-                        color="primary"
-                      />
-                    )}
-                    label={metricLabelName}
-                  />
-                );
-              }
-            )}
-          </FormGroup>
-        </FormControl>
+        <Metrics onStateChange={onMetricSelect} />
 
         {/* Cards */}
         <div className={classes.cardsContainer}>
           {
-            state.map((card) => {
+            Array.isArray(selectedMetrics) && selectedMetrics.map((card) => {
               const metricName = card;
               const metricLabelName = metricsData[metricName];
 
@@ -160,7 +85,7 @@ const Dashboard = () => {
 
       {/* Chart */}
       {
-        Array.isArray(state) && state.length ? (
+        Array.isArray(selectedMetrics) && selectedMetrics.length ? (
           <ResponsiveContainer>
             <LineChart data={mockChartData}>
               <CartesianGrid strokeDasharray="3 3"/>
